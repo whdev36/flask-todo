f768e2ae1add8c8fa4f944af5f0c6d53ad88b4ed
whdev36 <wisdomhunter36@gmail.com>
2024-11-24 16:15:19 -0800

    pages and urls

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from .config import Config
 import os
 import colorama
 
 database = SQLAlchemy()
+config = Config
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
-    app.config.from_object(Config)
+    app.config.from_object(config)
     database.init_app(app)
+
+    from .views import views
+    app.register_blueprint(views)
 
     colorama.init(autoreset=True)
 
-    if not os.path.exists(Config.DATABASE_NAME):
+    if not os.path.exists(config.DATABASE_NAME):
         with app.app_context():
             database.create_all()
-        print(colorama.Fore.GREEN + f'{Config.DATABASE_NAME} created successfully!')
+        print(colorama.Fore.GREEN + f'{config.DATABASE_NAME} created successfully!')
 
     return app
